e90259d51249add08542727a517584ea7671f552
denibro <denisdewdew@gmail.com>
2020-09-25 10:53:02 +0700

    dikantor

---
+++ resources/views/siswa/edit.blade.php
@@ -0,0 +1,107 @@
+@extends('layouts.master')
+
+
+@section('content')
+
+<div class="main">
+    <div class="main-content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+
+                    <div class="panel">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Update Data Siswa</h3>
+                        </div>
+                        <div class="panel-body">
+                            <form action="/siswa/{{ $siswa->id }}/update" method="POST">
+                                {{ csrf_field() }}
+                                <div class="form-group">
+                                    <label for="namadepan">Nama Depan</label>
+                                    <input type="text" class="form-control" id="namadepan" aria-describedby="emailHelp" placeholder="Nama Depan" name="nama_depan" value="{{ $siswa->nama_depan }}">
+                                    <small id="emailHelp" class="form-text text-muted"></small>
+                                </div>
+                                <div class="form-group">
+                                    <label for="namabelakang">Nama Belakang</label>
+                                    <input type="text" class="form-control" id="namabelakang" aria-describedby="emailHelp" placeholder="Nama Belakang" name="nama_belakang" value="{{ $siswa->nama_belakang }}">
+                                    <small id="emailHelp" class="form-text text-muted"></small>
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleFormControlSelect1">Jenis Kelamin</label>
+                                    <select class="form-control" id="exampleFormControlSelect1" name="jenis_kelamin">
+                                    <option value="L" @if ($siswa->jenis_kelamin == 'L') selected @endif>Laki Laki</option>
+                                    <option value="P" @if ($siswa->jenis_kelamin == 'P') selected @endif>Perempuan</option>
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label for="agama">Agama</label>
+                                    <input type="text" class="form-control" id="agama" aria-describedby="emailHelp" placeholder="Agama" name="agama" value="{{ $siswa->agama }}">
+                                    <small id="emailHelp" class="form-text text-muted"></small>
+                                </div>
+                                <div class="form-group">
+                                    <label for="alamat">Alamat</label>
+                                    <textarea class="form-control" id="alamat" rows="2" name="alamat">{{ $siswa->alamat }}</textarea>
+                                </div>
+                                    <button type="submit" class="btn btn-warning">Update</button>
+                                    <a href="/siswa" class="btn btn-info btn-md">Cencel</a>
+                            </form>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@stop
+
+
+@section('content1')
+
+    <h1>Edit Data Siswa</h1>
+    @if (session('sukses'))
+    <div class="alert alert-success" role="alert">
+        {{ session('sukses') }}
+    </div>
+    @endif
+
+    <div class="row">
+        <div class="col">
+            <form action="/siswa/{{ $siswa->id }}/update" method="POST">
+                {{ csrf_field() }}
+                <div class="form-group">
+                    <label for="namadepan">Nama Depan</label>
+                    <input type="text" class="form-control" id="namadepan" aria-describedby="emailHelp" placeholder="Nama Depan" name="nama_depan" value="{{ $siswa->nama_depan }}">
+                    <small id="emailHelp" class="form-text text-muted"></small>
+                </div>
+                <div class="form-group">
+                    <label for="namabelakang">Nama Belakang</label>
+                    <input type="text" class="form-control" id="namabelakang" aria-describedby="emailHelp" placeholder="Nama Belakang" name="nama_belakang" value="{{ $siswa->nama_belakang }}">
+                    <small id="emailHelp" class="form-text text-muted"></small>
+                </div>
+                <div class="form-group">
+                    <label for="exampleFormControlSelect1">Jenis Kelamin</label>
+                    <select class="form-control" id="exampleFormControlSelect1" name="jenis_kelamin">
+                    <option value="L" @if ($siswa->jenis_kelamin == 'L') selected @endif>Laki Laki</option>
+                    <option value="P" @if ($siswa->jenis_kelamin == 'P') selected @endif>Perempuan</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="agama">Agama</label>
+                    <input type="text" class="form-control" id="agama" aria-describedby="emailHelp" placeholder="Agama" name="agama" value="{{ $siswa->agama }}">
+                    <small id="emailHelp" class="form-text text-muted"></small>
+                </div>
+                <div class="form-group">
+                    <label for="alamat">Alamat</label>
+                    <textarea class="form-control" id="alamat" rows="2" name="alamat">{{ $siswa->alamat }}</textarea>
+                </div>
+                    <button type="submit" class="btn btn-primary">Update</button>
+                    <a href="/siswa" class="btn btn-secondary btn-md">Cencel</a>
+            </form>
+        </div>
+    </div>
+
+@endsection
+
+
+
